21c9d5dd392efe9fbaa0106f8e9243d2359c38d1
Taliaa <taliaa10@ymail.com>
2019-07-27 23:52:13 -0500

    build recipe controller

--- src/js/index.js
+++ src/js/index.js
@@ -29,12 +29,18 @@ const controlSearch = async () => {
         searchView.clearResults()
         renderLoader(elements.searchRes)
 
-        // 4) Search for recipes
-        await state.search.getResults()
+        try {
+            // 4) Search for recipes
+            await state.search.getResults()
 
-        // 5) render results on UI
-        clearLoader()
-        searchView.renderResults(state.search.result)
+            // 5) render results on UI
+            clearLoader()
+            searchView.renderResults(state.search.result)
+        } catch(err) {
+            alert('Something is wrong with the search...')
+            clearLoader()
+        }
+
     }
 }
 
@@ -63,8 +69,36 @@ elements.searchRes.addEventListener('click', e => {
 
 
 /**
- * SEARCH CONTROLLER
+ * RECIPE CONTROLLER
  */
-// const r = new Recipe(47746)
-// r.getRecipe()
-// console.log(r)
+const controlRecipe = async () => {
+    // Get ID from url
+    const id = window.location.hash.replace('#', '')
+    console.log(id)
+
+    if (id) {
+        // Prepare UI for changes
+
+        // Create new recipe object
+        state.recipe = new Recipe(id)
+
+        try {
+            // Get recipe data
+            await state.recipe.getRecipe()
+
+            // Calculate servings and time
+            state.recipe.calcTime()
+            state.recipe.calcServings()
+
+            //Render recipe
+            console.log(state.recipe)
+        } catch (err) {
+            alert('Error procsessing recipe!')
+        }
+
+        
+    }
+}
+
+//  adding the same event listener to different events
+['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe))
